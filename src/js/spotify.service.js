d8c6c6d94f0742a88663a5c3ff79870148aaa682
cordenbrock <cordenbrock@gmail.com>
2020-12-03 09:38:27 -0800

    make it better

--- src/js/spotify.service.js
+++ src/js/spotify.service.js
@@ -20,7 +20,7 @@ export default class Spotify {
 
   static searchSpotify(keyword) {
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${process.env.TOKEN}`);
+    myHeaders.append("Authorization", `Bearer BQBnp23h0K44CcPerK_hKXgyJhdjxp7IolB92RCtZpyLjM0FSE-yv1YXUI_rgket4TbR_fOS_joHQjAYpbw3O-JLC8Gcs6tyvJ4oVigcflAtwJI-FlyQH4T98vXqsEdCEVboZlsRkK0cer9gG5ve-IiQd169_KBE`);
     myHeaders.append("Cookie", "_ga=GA1.2.456045822.1606765238; _gid=GA1.2.637614845.1606765238; sp_dc=AQB5RMpfdYpkmkJlRHPeYxSSfyTRPpkKySEvxhbkmixFgyFT0ZOlFeh5KY4xSVAXFpCp276qTTx4nbxD11fqn-6DnKhR9PuJ6yLMzkXUPg; sp_key=66b31764-29a3-4c2f-92c8-38ed42c7b352; _gat=1");
 
     const requestOptions = {
