d428d028d4a556b88294ab9c4b57420363c9c354
cordenbrock <cordenbrock@gmail.com>
2020-12-03 12:53:08 -0800

    New access token

--- src/js/spotify.service.js
+++ src/js/spotify.service.js
@@ -10,7 +10,7 @@ export default class Spotify {
 
   static searchSpotify(keyword) {
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer BQChmzuXSw8geQrrUuaFDO7-QSTtMtQ08bhLOMBva6TsHvgRLeZhGNu9_KvpkKlPLQkYYDclNwOZ8uSbGI33lJZXPNXwQkPVgLJWXP7NUNyka2TzlQ7ureWSmka3TBv7JNN9Bs9syXsencUcAgbTyA0Ei0qFqgVH`);
+    myHeaders.append("Authorization", `Bearer BQDyAVBH4W-9IJtU2JVS5fRnu6wONUgUnJpJ3m2BnAV5U15-M9UvB02aWIAg9POcV4x7q2T6dTzeM8R0M_RB3MkPpZRCQqouxsSpKq1d4vF-jsXgUx26GtPojCppHgw6c9xD2gUGx1agTs99Ecm8U5CKFroY8Koc`);
     myHeaders.append("Cookie", "_ga=GA1.2.456045822.1606765238; _gid=GA1.2.637614845.1606765238; sp_dc=AQB5RMpfdYpkmkJlRHPeYxSSfyTRPpkKySEvxhbkmixFgyFT0ZOlFeh5KY4xSVAXFpCp276qTTx4nbxD11fqn-6DnKhR9PuJ6yLMzkXUPg; sp_key=66b31764-29a3-4c2f-92c8-38ed42c7b352; _gat=1");
 
     const requestOptions = {
